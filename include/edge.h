df2a2ddee545aa83d12e142b8f6730e1b66340d2
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-04-05 17:03:07 +0530

    Change newpoint to glm::vec3

--- include/edge.h
+++ include/edge.h
@@ -1,12 +1,10 @@
 #ifndef EDGE_H
 #define EDGE_H
 
-/*
 //careful: no multiple declarations for glm allowed
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-*/
 
 #include <vector>
 #include <cmath>
@@ -21,7 +19,7 @@ class Edge
     public:
         unsigned int u;
         unsigned int v;
-        unsigned int newPoint;   //midpoint to split edge
+        glm::vec3 newPoint;   //midpoint to split edge
         unsigned int sharedPoint1;   //third point of first triangle in the edge
         unsigned int sharedPoint2 = 0;   //third point of second triangle in the edge
 
