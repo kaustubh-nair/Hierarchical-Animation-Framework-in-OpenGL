db210227ac4d00d44cf9024f12e28c3f9c52e00e
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-04-29 16:41:37 +0530

    Cleanup main

--- source/main.cpp
+++ source/main.cpp
@@ -10,7 +10,7 @@
 
 void setupMeshes(std::vector<std::string> &filepaths,
                  std::vector<glm::vec3> &meshPos,
-                 std::string &texturePath, int scene);
+                 std::string &texturePath);
 
 int main()
 {
@@ -21,15 +21,7 @@ int main()
     std::vector<glm::vec3> meshPos;
     std::string texturePath;
 
-    /* meshes for three parts in different model instances (model == scene)*/
-    /* TODO: multi thread?? */
-    setupMeshes(filepaths, meshPos, texturePath, SCENE_1);
-    controller.setupScene(filepaths, meshPos, texturePath);
-
-    setupMeshes(filepaths, meshPos, texturePath, SCENE_2);
-    controller.setupScene(filepaths, meshPos, texturePath);
-
-    setupMeshes(filepaths, meshPos, texturePath, SCENE_3);
+    setupMeshes(filepaths, meshPos, texturePath);
     controller.setupScene(filepaths, meshPos, texturePath);
 
     controller.mainLoop();
@@ -40,55 +32,19 @@ int main()
 
 void setupMeshes(std::vector<std::string> &filepaths,
                  std::vector<glm::vec3> &meshPos,
-                 std::string &texturePath, int scene)
+                 std::string &texturePath)
 {
-
 
     texturePath = "data/checkerboard.jpg";
     filepaths.clear();
     meshPos.clear();
-    switch(scene)
-    {
-        case SCENE_1:
-            {
-                // loading 9 spheres
-                float dist = 1.2f;
-                for(int i = -1; i < 2; i++)
-                {
-                    for(int j = -1; j < 2; j++)
-                    {
-                        filepaths.push_back("data/sphere.ply");
-                        meshPos.push_back(glm::vec3(dist * i, dist * j, -5.0f));
-                    }
-                }
-                break;
-            }
+    filepaths.push_back("data/bun_zipper.ply");
+    filepaths.push_back("data/teapot.ply");
+    filepaths.push_back("data/cylinder.ply");
+    filepaths.push_back("data/octahedron.ply");
 
-        case SCENE_2:
-            {
-                filepaths.push_back("data/bun_zipper.ply");
-                filepaths.push_back("data/teapot.ply");
-                filepaths.push_back("data/cylinder.ply");
-                filepaths.push_back("data/octahedron.ply");
-
-                meshPos.push_back(glm::vec3(1.0f, 1.0f, -1.0f));
-                meshPos.push_back(glm::vec3(1.0f, 0.0f, -1.0f));
-                meshPos.push_back(glm::vec3(0.0f, 1.0f, -1.0f));
-                meshPos.push_back(glm::vec3(0.0f, 0.0f, -1.0f));
-
-                break;
-            }
-
-        case SCENE_3:
-            {
-                filepaths.push_back("data/octahedron.ply");
-                filepaths.push_back("data/tetrahedron.ply");
-
-                meshPos.push_back(glm::vec3(1.0f, 0.0f, -6.0f));
-                meshPos.push_back(glm::vec3(1.0f, 1.0f, -6.0f));
-                break;
-            }
-                
-    }
-
+    meshPos.push_back(glm::vec3(1.0f, 1.0f, -1.0f));
+    meshPos.push_back(glm::vec3(1.0f, 0.0f, -1.0f));
+    meshPos.push_back(glm::vec3(0.0f, 1.0f, -1.0f));
+    meshPos.push_back(glm::vec3(0.0f, 0.0f, -1.0f));
 }
