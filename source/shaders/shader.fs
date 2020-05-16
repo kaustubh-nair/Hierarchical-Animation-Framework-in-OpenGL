ce00d3f59300b7150e369b94f685163effe041e6
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-05-16 18:46:34 +0530

    light_node: Add diffuse and ambient strengths

--- source/shaders/shader.fs
+++ source/shaders/shader.fs
@@ -7,7 +7,6 @@ in vec3 Normal;
 in vec3 FragPos;  
 in vec2 TexCoord;
   
-uniform vec3 objectColor;
 uniform sampler2D Texture;
 uniform vec3 viewPos;
 
@@ -25,31 +24,33 @@ void main()
     vec3 lightColor = vec3(texture(Texture, TexCoord));
 
     vec3 result = vec3(0.0f, 0.0f, 0.0f);
+
     for(int i = 0; i < NUMBER_OF_LIGHTS; i++)
     {
-    vec3 lightPos = lights[i].Pos;
-    float dist = distance(lightPos, FragPos);
-    float difDist = 10.0f/pow(dist, 2);
-    float diffuseStrength = lights[i].diffuseStrength;
+        vec3 lightPos = lights[i].Pos;
+        float dist = distance(lightPos, FragPos);
+        float difDist = 10.0f/pow(dist, 2);
+        float diffuseStrength = lights[i].diffuseStrength;
 
-    // ambient
-    vec3 ambient = ambientStrength * lightColor;
+        // ambient
+        vec3 ambient = ambientStrength * lightColor;
 
-    // diffuse 
-    vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
-    float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = difDist*diff * lightColor;
+        // diffuse 
+        vec3 norm = normalize(Normal);
+        vec3 lightDir = normalize(lightPos - FragPos);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = difDist*diff * lightColor;
 
-    //specular
-    float specDist = (16.5f - dist)/6.5f;
-    float specularStrength = lights[i].specularStrength;
-    vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specDist*specularStrength * spec * lightColor;  
-    result += (ambient+ specular+ diffuse);
+        //specular
+        float specDist = (16.5f - dist)/6.5f;
+        float specularStrength = lights[i].specularStrength;
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);  
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+        vec3 specular = specDist*specularStrength * spec * lightColor;  
+        result += (ambient+ specular+ diffuse);
 
-}
-    FragColor =  vec4(result * objectColor, 1.0f);
+    }
+
+    FragColor =  vec4(result, 1.0f);
 }
