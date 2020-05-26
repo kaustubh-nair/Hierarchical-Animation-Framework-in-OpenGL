c06564c7a025dfff5bc5ed56d17e70e97a662514
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-05-26 17:08:05 +0530

    main: Mesh adjustments

--- source/main.cpp
+++ source/main.cpp
@@ -11,7 +11,7 @@
 
 // problems
 // position in mesh_node needs to be update and kept absolute, even though you're only passing relative positions while creation.
-// connection needs to be added between camera and person for move around, but camera and head for lookaround
+// connection needs to be added between camera and personA for move around, but camera and head for lookaround
 int main()
 {
     glm::vec3 origin = glm::vec3(0.0f, 0.0f, 0.0f);
@@ -32,33 +32,57 @@ int main()
     SceneNode *cameraGroup = controller.model.addGroupNode(2, 0);   //cameras
     SceneNode *meshGroup = controller.model.addGroupNode(3, 0);   //meshes
 
-    /* NOTE: Camera nodes should be inserted sequentially */
-    SceneNode *camera1 = controller.model.addCameraNode(4, 2, origin + (5.0f*z) + x + y, -z, y);
-    SceneNode *camera2 = controller.model.addCameraNode(5, 2, origin - (10.0f*x), x, y);
+    /* NOTE: Camera nodes should be inserted with sequential ids */
 
-
-
+    /* main camera */
+    SceneNode *camera = controller.model.addCameraNode(4, 2, origin + (5.0f*z) + x + y, -z, y);
     SceneNode *grass = controller.model.addMeshNode(9, 3, "data/meshes/rectangle.ply", "data/textures/grass.jpg", CYLINDERICAL_MAPPING,
                                                     origin, glm::scale(mat, 40.0f * unit), mat);
 
 
-    SceneNode *person = controller.model.addMeshNode(6, 3, "data/meshes/body.ply", "data/textures/skin.jpg", CYLINDERICAL_MAPPING,
+
+    SceneNode *personA = controller.model.addMeshNode(10, 3, "data/meshes/body.ply", "data/textures/skin.jpg", CYLINDERICAL_MAPPING,
+                                                      x + (0.5f*y), mat, mat);
+
+    SceneNode *headA = controller.model.addMeshNode(11, 10, "data/meshes/sphere.ply", "data/textures/face.jpg", SPHERICAL_MAPPING,
+                                                    1.5f*y, glm::scale(mat, 0.12f*unit), glm::rotate(mat, glm::radians(-90.0f), y));
+    SceneNode *leftHmdA = controller.model.addCameraNode(5, 2, origin - (10.0f*x), x, y);
+    SceneNode *rightHmdA = controller.model.addCameraNode(6, 2, origin - (10.0f*x), x, y);
+    controller.model.addConnection(personA, leftHmdA);
+    controller.model.addConnection(personA, rightHmdA);
+
+
+
+    SceneNode *personB = controller.model.addMeshNode(12, 3, "data/meshes/body.ply", "data/textures/skin.jpg", CYLINDERICAL_MAPPING,
                                                      x + (0.5f*y), mat, mat);
 
-    SceneNode *head = controller.model.addMeshNode(10, 6, "data/meshes/sphere.ply", "data/textures/face.jpg", SPHERICAL_MAPPING,
-                                                   (1.5f*y), glm::scale(mat, 0.12f*unit), glm::rotate(mat, glm::radians(-90.0f), y));
+    SceneNode *headB = controller.model.addMeshNode(13, 12, "data/meshes/sphere.ply", "data/textures/face.jpg", SPHERICAL_MAPPING,
+                                                   1.5f*y, glm::scale(mat, 0.12f*unit), glm::rotate(mat, glm::radians(-90.0f), y));
+    SceneNode *leftHmdB = controller.model.addCameraNode(7, 2, origin - (10.0f*x), x, y);
+    SceneNode *rightHmdB = controller.model.addCameraNode(8, 2, origin - (10.0f*x), x, y);
+    controller.model.addConnection(personA, leftHmdB);
+    controller.model.addConnection(personA, rightHmdB);
 
-    SceneNode *balloon = controller.model.addMeshNode(7, 3, "data/meshes/sphere.ply", "data/textures/red.jpg", CYLINDERICAL_MAPPING,
+
+
+    SceneNode *personC = controller.model.addMeshNode(14, 3, "data/meshes/body.ply", "data/textures/skin.jpg", CYLINDERICAL_MAPPING,
+                                                     x + (0.5f*y), mat, mat);
+
+    SceneNode *headC = controller.model.addMeshNode(15, 14, "data/meshes/sphere.ply", "data/textures/face.jpg", SPHERICAL_MAPPING,
+                                                   1.5f*y, glm::scale(mat, 0.12f*unit), glm::rotate(mat, glm::radians(-90.0f), y));
+
+
+    SceneNode *balloon = controller.model.addMeshNode(16, 3, "data/meshes/sphere.ply", "data/textures/red.jpg", CYLINDERICAL_MAPPING,
                                                      -z, glm::scale(mat, 4.0f*unit), mat);
 
-    SceneNode *basket = controller.model.addMeshNode(8, 7, "data/meshes/cube.ply", "data/textures/skin.jpg", CYLINDERICAL_MAPPING,
+    SceneNode *basket = controller.model.addMeshNode(17, 16, "data/meshes/cube.ply", "data/textures/skin.jpg", CYLINDERICAL_MAPPING,
                                                      -(1.7f*y), mat, mat);
 
-    SceneNode *animal = controller.model.addMeshNode(11, 7, "data/meshes/cow.ply", "data/textures/black.jpg", CYLINDERICAL_MAPPING,
+    SceneNode *animal = controller.model.addMeshNode(18, 16, "data/meshes/cow.ply", "data/textures/black.jpg", CYLINDERICAL_MAPPING,
                                                      -(1.7f*y) + (0.3f*x), mat, mat);
     
 
-    SceneNode *bird = controller.model.addMeshNode(12, 3, "data/meshes/humbird.ply", "data/textures/feather.jpg", CYLINDERICAL_MAPPING,
+    SceneNode *bird = controller.model.addMeshNode(19, 3, "data/meshes/humbird.ply", "data/textures/feather.jpg", CYLINDERICAL_MAPPING,
                                                    -x, glm::scale(mat, 0.5f*unit), mat);
 
     /*
@@ -74,8 +98,6 @@ int main()
     SceneNode *light1 = controller.model.addLightNode(13, 1, origin + (4.0f*y) + x);
     SceneNode *light2 = controller.model.addLightNode(14, 1, origin + (4.0f*y) - x);
 
-    // Connections are traversed during update only.
-    controller.model.addConnection(person, camera1);
 
 
     controller.run();
